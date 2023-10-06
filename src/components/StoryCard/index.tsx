f4d7ffc863dd9e04d60d465fbb8f5fac745dbb22
Faishal Pasa <faishal.pasa@bareksa.com>
2023-10-06 19:14:15 +0700

    fet: add env

--- src/components/StoryCard/index.tsx
+++ src/components/StoryCard/index.tsx
@@ -10,10 +10,10 @@ const StoryCard = () => {
 
   return (
     <div className={classes.card}>
-      <img src="/images/cat-1.png" alt="star1" className={classes.star1} />
-      <img src="/images/cat-1.png" alt="star2" className={classes.star2} />
-      <img src="/images/cat-1.png" alt="star3" className={classes.star3} />
-      <img src="/images/cat-1.png" alt="star4" className={classes.star4} />
+      <img src={`${process.env.REACT_APP_ASSET_URL}/images/cat-1.png`} alt="star1" className={classes.star1} />
+      <img src={`${process.env.REACT_APP_ASSET_URL}/images/cat-1.png`} alt="star2" className={classes.star2} />
+      <img src={`${process.env.REACT_APP_ASSET_URL}/images/cat-1.png`} alt="star3" className={classes.star3} />
+      <img src={`${process.env.REACT_APP_ASSET_URL}/images/cat-1.png`} alt="star4" className={classes.star4} />
 
       <div className={classes.titleOne}>
         <div className={classes.titleContent}>
@@ -30,16 +30,16 @@ const StoryCard = () => {
       <div className={classes.titleTwo}>
         <div className={classes.titleContent}>
           <div className={classes.imageShuffle}>
-            <img src="/images/cat-2.png" alt="shuffle1" className={classes.imageShuffle1} />
+            <img src={`${process.env.REACT_APP_ASSET_URL}/images/cat-2.png`} alt="shuffle1" className={classes.imageShuffle1} />
           </div>
           <div className={classes.imageShuffle}>
-            <img src="/images/cat-3.png" alt="shuffle2" className={classes.imageShuffle2} />
+            <img src={`${process.env.REACT_APP_ASSET_URL}/images/cat-3.png`} alt="shuffle2" className={classes.imageShuffle2} />
           </div>
           <div className={classes.imageShuffle}>
-            <img src="/images/cat-4.png" alt="shuffle3" className={classes.imageShuffle3} />
+            <img src={`${process.env.REACT_APP_ASSET_URL}/images/cat-4.png`} alt="shuffle3" className={classes.imageShuffle3} />
           </div>
           <div className={classes.imageShuffle}>
-            <img src="/images/cat-5.png" alt="shuffle4" className={classes.imageShuffle4} />
+            <img src={`${process.env.REACT_APP_ASSET_URL}/images/cat-5.png`} alt="shuffle4" className={classes.imageShuffle4} />
           </div>
 
           <p className={classes.textHeaderTwo}>Proin sit amet dolor quis tortor tempor faucibus</p>
@@ -48,7 +48,7 @@ const StoryCard = () => {
 
       <div className={classes.titleThree}>
         <div className={classes.titleContent}>
-          <img src="/images/cat-6.png" alt="result" className={classes.imageResult} />
+          <img src={`${process.env.REACT_APP_ASSET_URL}/images/cat-6.png`} alt="result" className={classes.imageResult} />
           <p className={classes.textHeaderThree}>The Specialist</p>
           <p className={classes.textDescriptionThree}>
             Pellentesque mattis, libero ut maximus rutrum, lectus quam hendrerit neque,
